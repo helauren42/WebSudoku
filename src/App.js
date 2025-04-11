a8b91ed7222188ef3dd78dcc4eb7416dd8b21c13
helauren <helauren42@gmail.com>
2025-04-11 21:20:47 +0200

    better looking navbar, hover active added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import { useState, useEffect } from 'react';
-
+import { Navbar, setUserSectionWidth } from './Navbar';
 import './App.css'
-import { PROJECT_SRC } from './Const.js'
-import UserIcon from './imgs/user.svg'
-console.log(PROJECT_SRC)
 
-const userIcon = `${PROJECT_SRC}imgs/user.svg`
-
-console.log(userIcon)
-
-const Navbar = () => {
-	const [loggedin, setLoggedin] = useState(false)
+const Board = () => {
+	const [active, setActive] = useState(false)
 
 	return (
-		<div id="navbar">
-			<h1 id="title">Sudoku</h1>
-			<div id="nav-menu">
-				<button className='nav-button'>Play</button>
-				<button className='nav-button'>Rankings</button>
-			</div>
-			<div id="user-section">
-				<img id="user-icon" src={UserIcon}></img>
-				<p id="">{loggedin ? 'account' : 'login'}</p>
-			</div>
-		</div >
+		<div>
+
+		</div>
 	)
+
 }
 
 const App = () => {
 	return (
-		<Navbar />
+		<>
+			<Navbar />
+			<Board />
+		</>
 	)
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import './Navbar.css'
+import UserIcon from './imgs/user.svg'
+import { PROJECT_SRC } from './Const.js'
+import { useState, useEffect } from 'react';
+
+const userIcon = `${PROJECT_SRC}imgs/user.svg`
+
+console.log(userIcon)
+
+function setUserSectionWidth() {
+	let user_section = document.getElementById('user-section')
+	let title = document.getElementById('title')
+	let titleRect = title.getBoundingClientRect()
+	let width = titleRect.width
+	// user_section.setAttribute('width', width)
+	user_section.style.width = `${width}px`;
+}
+
+export const Navbar = () => {
+	const [loggedin, setLoggedin] = useState(false)
+
+	useEffect(() => {
+		setUserSectionWidth()
+		window.addEventListener('resize', setUserSectionWidth)
+		return () => {
+			window.removeEventListener('resize', setUserSectionWidth)
+		}
+	}, [])
+
+	return (
+		<div id="navbar">
+			<h1 id="title">Sudoku</h1>
+			<div id="nav-menu">
+				<button className='nav-button'>Rankings</button>
+				<button className='nav-button'>Play</button>
+			</div>
+			<div id="user-section">
+				<img id="user-icon" src={UserIcon}></img>
+				<p id="user-text">{loggedin ? 'account' : 'login'}</p>
+			</div>
+		</div >
+	)
+}
+
+// export { Navbar }
