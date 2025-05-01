11f2adca18414f25276af31c1661f94e60827ca9
helauren <helauren42@gmail.com>
2025-05-01 12:07:06 +0200

    manual multiprocessing of makePuzzle

--- backend/makePuzzles.py
+++ backend/makePuzzles.py
@@ -1,7 +1,7 @@
 from sudokuGenerator.sudoku_generator import getPuzzle
 from const import PROJECT_DIR
 import subprocess
-import threading
+import sys
 
 LEVELS = ("easy", "medium", "hard", "extreme")
 BACKEND_DIR = f"{PROJECT_DIR}backend/"
@@ -25,26 +25,45 @@ class makePuzzles():
                     writeFile.write(puzzle)
                 print("done")
     @staticmethod
-    def startThreads():
-        one = threading.Thread(target=makePuzzles.makePuzzle, args=(1, 200))
-        two = threading.Thread(target=makePuzzles.makePuzzle, args=(201, 400))
-        three = threading.Thread(target=makePuzzles.makePuzzle, args=(401, 600))
-        four = threading.Thread(target=makePuzzles.makePuzzle, args=(601, 800))
-        five = threading.Thread(target=makePuzzles.makePuzzle, args=(801, 1000))
-        one.start()
-        two.start()
-        three.start()
-        four.start()
-        five.start()
-        one.join()
-        two.join()
-        three.join()
-        four.join()
-        five.join()
+    def findMin(start, end):
+        output = subprocess.run(["ls"], capture_output=True, cwd=(f"{BACKEND_DIR}puzzles/extreme")).stdout.decode()
+        filenames = output.split("\n")
+        num = start
+        while(num >= start and num <= end):
+            searchName = f"{num}.txt"
+            if searchName not in filenames:
+                break;
+            num+=1
+        return num
+    @staticmethod
+    def allEasyPuzzles():
+        for num in range(1, 1001):
+            print(num)
+            puzzle = getPuzzle("easy", f"{BACKEND_DIR}basePuzzles/{num}.txt")
+            with open(f"{BACKEND_DIR}puzzles/easy/{num}.txt", "w") as writeFile:
+                writeFile.write(puzzle)
+            print(f"{num} done")
 
 def main():
     makePuzzles.setupDirs()
-    makePuzzles.startThreads()
+    if sys.argv[1] == "easy":
+        makePuzzles.allEasyPuzzles()
+        sys.exit(0)
+    min = int(sys.argv[1])
+    max = int(sys.argv[2])
+    min = makePuzzles.findMin(min, max)
+    if min == max+1:
+        print('already done')
+    print("min: ", min)
+    makePuzzles.makePuzzle(min, max)
+    print('finished')
 
 main()
 
+# execute the script in parallel like this:
+# python3 makePuzzles.py 1 100
+# python3 makePuzzles.py 101 200
+# python3 makePuzzles.py 201 300
+# python3 makePuzzles.py 301 400
+# python3 makePuzzles.py 401 500
+# etc..
