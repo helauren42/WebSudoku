33b9e16deb7bea57197284546dd0f3c6a01727ba
helauren <helauren42@gmail.com>
2025-05-05 04:13:34 +0200

    refactored rankings reset services

--- backend/Database/removeService.sh
+++ backend/Database/removeService.sh
@@ -2,7 +2,8 @@ sudo systemctl disable --now dailyReset.timer
 sudo systemctl disable --now weeklyReset.timer
 sudo systemctl daemon-reload
 
+rm -rf builtServices
 sudo rm /etc/systemd/system/dailyReset.service
-sudo rm /etc/systemd/system/dailyReset.service
-sudo rm /etc/systemd/system/dailyReset.service
-sudo rm /etc/systemd/system/dailyReset.service
+sudo rm /etc/systemd/system/weeklyReset.service
+sudo rm /etc/systemd/system/dailyReset.timer
+sudo rm /etc/systemd/system/weeklyReset.timer
